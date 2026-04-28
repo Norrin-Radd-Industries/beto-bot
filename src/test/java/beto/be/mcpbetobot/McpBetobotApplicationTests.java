2ad6af248f68643dfb0650da832d8f24699bb497
BetoBot-1 <umberto.asu+beto-bot@gmail.com>
2026-04-28 22:46:42 +0200

    Add basic application context load test

---
+++ src/test/java/beto/be/mcpbetobot/McpBetobotApplicationTests.java
@@ -0,0 +1,15 @@
+package beto.be.mcpbetobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class McpBetobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
